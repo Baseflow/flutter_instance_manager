11f6057b6a00753f2f32d2aca47fa654ef9defb4
tim Hoogstrate <tim566@hotmail.com>
2024-03-15 13:19:51 +0100

    updates

--- lib/flutter_instance_manager.dart
+++ lib/flutter_instance_manager.dart
@@ -1,5 +1,3 @@
 library flutter_instance_manager;
 
-export 'src/instance_manager.dart';
-export 'src/android_instance_manager.dart';
-export 'test/instance_manager_test.mocks.dart';
+export 'src/shared_instance_manager.dart';
